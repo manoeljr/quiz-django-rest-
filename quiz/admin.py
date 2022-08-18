91932fbd445011c688a918675d86a82549fcd66b
manoeljr <manoelv8@gmail.com>
2022-08-17 22:33:32 -0300

    Configuração do arquivo admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz import models
+from quiz.models import Answer
+from quiz.models import Category
+from quiz.models import Question
+from quiz.models import Quizzes
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+@admin.register(Quizzes)
+class QuizzesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title',]
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = models.Answer
+    fields = ['answer_text', 'is_right']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['title', 'quiz']
+    list_display = ['title', 'quiz', 'date_updated']
+    inlines = [AnswerInlineModel,]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['answer_text', 'is_right', 'question']
